767cbefd1bc17a354ee5601ded5c63c92556306a
“mrZAC” <mrZAC@mail.ru>
2023-06-07 19:36:04 +0300

    changes app add routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Jenkins! This is my updated application.');
+});
+
+app.get('/test', (req, res) => {
+  res.send('This is a test route.');
 });
 
 app.listen(port, () => {
